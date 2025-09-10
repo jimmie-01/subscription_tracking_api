d5826ba0fdc62579d8c3645b5785e03537ef0734
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-09-10 23:03:20 +0100

    Basic Express App

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 import express from 'express';
 
 const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Welcome to the Subscription Tracker API');
+});
+
+app.listen(port: 3000, () => {
+	console.log('server running on port 3000');
+});
+
+export default app;
