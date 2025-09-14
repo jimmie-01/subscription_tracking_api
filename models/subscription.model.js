265d5d107c7413fdf2fde507913c02f2d58fa780
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-09-14 11:09:21 +0100

    Subscription DB Model

--- models/subscription.model.js
+++ models/subscription.model.js
@@ -1,5 +1,64 @@
 import mongoose from 'mongoose';
 
 const subscriptionSchema = new mongoose.Schema({
-	
-})
+	name: {
+		type: String,
+		required: [true, 'Subscription name is required'],
+		trim: true,
+		minLength: 2,
+		maxLength: 100
+	},
+	price: {
+		type: Number,
+		required: [true, 'Subscription price is required'],
+		min: [0, 'Price must be greater than 0'],
+	},
+	currency: {
+		type: String,
+		enum: ['USD', 'GBP', 'EURO', 'NGN'],
+		default: 'NGN',
+	},
+	fequency: {
+		type: String,
+		enum: ['daily', 'weekly', 'monthly', 'yearly'],
+	},
+	category: {
+		type: String,
+		enum: ['sports', 'news', 'entertainment', 'lifestyle', 'technology', 'finance', 'politics', 'other'],
+		required: true
+	},
+	paymentMethod: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	status: {
+		type: String,
+		enum: ['active', 'cancelled', 'paused', 'expired'],
+		default: 'active'
+	},
+	startDate: {
+		type: Date,
+		required: true,
+		validate: {
+			validator: (value) => value <= new Date(),
+			message: 'Start date must be in the past'
+		}
+
+	},
+	renewalDate: {
+		type: Date,
+		required: true,
+		validate: {
+			
+		}
+	},
+	renewalInterval: {
+		type: String,
+		enum: ['monthly', 'yearly', 'weekly', 'daily'],
+		default: 'monthly'
+	},
+	notes: {
+		type: String
+	}
+}, { timestamps: true });
