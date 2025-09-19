7bf8041c0ff57956484d66db20014f26812e1176
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-09-19 10:42:29 +0100

    User signup logic

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,16 @@
-export const signup = async (req, res, next) => {
+import bcrypt from 'bcryptjs';
+import User from '../models/user.model.js';
 
+export const signup = async (req, res, next) => {
+	const { email, password } = req.body;
+
+	try {
+		const hashedPassword = await bcrypt.hash(password, 10);
+		const newUser = await User.create({ email, password: hashedPassword });
+		res.status(201).json({ user: newUser });
+	} catch (error) {
+		next(error);
+	}
 }
 
 export const signin = async (req, res, next) => {
