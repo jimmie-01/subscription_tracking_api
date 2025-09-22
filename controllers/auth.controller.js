e0711920290e60e9aef8e3ce9e47e6d322b1a4cd
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-09-22 12:50:37 +0100

    USER SIGN-IN AND BASIC AUTHENTICATION  SERVER LOGIC

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -51,7 +51,39 @@ export const signUp = async (req, res, next) => {
 }
 
 export const signIn = async (req, res, next) => {
+	try {
+		const { email, password } = req.body;
 
+		// Check if user exists
+		const user = await User.findOne({ email });
+
+		if (!user) {
+			const error = new Error('User not found');
+			error.statusCode = 404;
+			throw error;
+		}
+
+		const isPasswordValid = await bcrypt.compare(password, user.password);
+
+		if (!isPasswordValid) {
+			const error = new Error('Invalid Password');
+			error.statusCode = 401;
+			throw error;
+		}
+
+		const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
+
+		res.status(200).json({
+			success: true,
+			message: 'User signed in successfully',
+			data: {
+				token,
+				user
+			}
+		});
+	} catch (error) {
+		next(error);
+	}
 }
 
 export const signOut = async (req, res, next) => {
