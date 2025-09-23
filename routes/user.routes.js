14aa25b49e85798e2a5405c314ab5e40a5bcc60f
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-09-23 14:12:58 +0100

    Authorization with jwt

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
+
+import authorize from '../middleware/auth.middleware.js';
 import { getUsers, getUser } from '../controllers/user.controller.js';
 
 const userRouter = Router();
 
 userRouter.get('/', getUsers);
 
-userRouter.get('/:id', getUser);
+userRouter.get('/:id', authorize, getUser);
 
 userRouter.post('/', (req, res) => {});
 
