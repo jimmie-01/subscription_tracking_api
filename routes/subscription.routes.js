03a4bebde2795e340543422d81d49e35e0f877da
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-09-24 12:32:06 +0100

    Server logic to create subscription

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import authorize from '../middleware/auth.middleware.js';
+import { createSubscription } from '../controllers/subscription.controller.js';
 
 const subRouter = Router();
 
@@ -6,7 +8,7 @@ subRouter.get('/', (req, res) => {});
 
 subRouter.get('/:id', (req, res) => {});
 
-subRouter.post('/', (req, res) => {});
+subRouter.post('/', authorize, createSubscription);
 
 subRouter.put('/:id', (req, res) => {});
 
