85c3615b6d0375a38f2dbf473041366edb3e37f5
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-09-25 23:41:54 +0100

    Subscription Model bug fix

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,3 +1,5 @@
+import Subscription from '../models/subscription.model.js'
+
 export const createSubscription = async (req, res, next) => {
 	try {
 		const subscription = await Subscription.create({
