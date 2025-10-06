c1df53d5158fcace3ee7d0a622de8ff08140fa89
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-10-06 10:52:42 +0100

    Auto Email reminder implementation with upstash

---
+++ config/upstash.js
@@ -0,0 +1,5 @@
+import { Client as WorkflowClient } from '@upstash/workflow';
+
+import { QSTASH_TOKEN, QSTASH_URL } from './env.js';
+
+export const workflowClient = new WorkflowClient(QSTASH_URL, QSTASH_TOKEN);
