7dafa6cda58c1a869f5e2162e58f5cae5ae5432e
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-10-10 11:52:24 +0100

    SendReminder Function

--- controllers/workflow.controller.js
+++ controllers/workflow.controller.js
@@ -2,6 +2,13 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const { serve } = require('@upstash/workflow/express');
 
-export const sendReminders = serve( async () => {
-	
-}); 
+export const sendReminders = serve( async (context) => {
+	const { subscriptionId } = context.requestPayload;
+	const subscription = await fetchSubscription(context, subscriptionId);
+});
+
+const fetchSubscription = async(context, subscriptionId) => {
+	return await context.run('get subscription', () => {
+
+	})
+}
